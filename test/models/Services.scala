000fb77cdd641086c5c6c131d2b8abf5c105333d
Oscar Renalias <oscar.renalias@accenture.com>
2012-07-07 01:33:08 +0300

    Added some tests

---
+++ test/models/Services.scala
@@ -0,0 +1,13 @@
+package models
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Oscar
+ * Date: 6/07/12
+ * Time: 14:32
+ * To change this template use File | Settings | File Templates.
+ */
+
+class Services {
+
+}
