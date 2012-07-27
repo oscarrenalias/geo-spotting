dfd20d696694ae3858007e164ce245bcbf79ed7b
Oscar Renalias <oscar.renalias@accenture.com>
2012-07-27 13:37:08 +0300

    Changed the application identifier.

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "ticket-spotting"
+    val appName         = "geo-spotting"
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
