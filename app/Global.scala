90fc6c5863031588df85118f78b6dc23c50ed9f7
Oscar Renalias <oscar.renalias@accenture.com>
2012-07-28 12:45:53 +0200

    Some debug logging.

--- app/Global.scala
+++ app/Global.scala
@@ -4,5 +4,7 @@ import play.api.{Logger, Application, GlobalSettings}
 object Global extends GlobalSettings {
   override def onStart(app: Application) {
     Logger.info("Starting application")
+    Logger.info("MongoDB URL = " + Store.mongoURI)
+    Logger.info("MongoDB URI object = " + Store.mongo.toString)
   }
 }
