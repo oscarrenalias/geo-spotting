332ece9a0e5ec0fdf2f06e657afe694f7909cb25
Oscar Renalias <oscar@renalias.net>
2012-08-06 00:01:32 +0300

    Small fix for MongoDB connection

--- app/helpers/Store.scala
+++ app/helpers/Store.scala
@@ -17,7 +17,7 @@ object Store {
     uri.connectDB.fold(
       error => { throw storeError(Some(error)); /* dummy mongo DB */ MongoDB(MongoConnection(""), "") },
       db => {
-	      Logger.debug("Mongo user = " + uri.username.getOrElse("") + ", password = " + uri.password.getOrElse("").toString)
+	      Logger.debug("Mongo user = " + uri.username.getOrElse("") + ", password = " + uri.password.getOrElse(Array()).mkString(""))
 	      db.authenticate(uri.username.getOrElse(""), uri.password.getOrElse("").toString); db }
     )
   }
